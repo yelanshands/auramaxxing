9f5364967e4e6704a4b0823d97898c4420622d14
yindy cam <cindyyam8@gmail.com>
2026-07-29 13:14:49 -0700

    logging

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-console.log('URL:', process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log('KEY:', process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY);
+console.log('URL:', process.env.REACT_APP_SUPABASE_URL);
+console.log('KEY:', process.env.REACT_APP_SUPABASE_PUBLISHABLE_KEY);
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_PUBLISHABLE_KEY;
