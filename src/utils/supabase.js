98e7b4cca488508baa631ce98b8ac439beced84f
yindy cam <cindyyam8@gmail.com>
2026-07-30 22:26:11 -0700

    fixed keys (again)

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-console.log('URL:', process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log('KEY:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+console.log('URL:', process.env.REACT_APP_SUPABASE_URL);
+console.log('KEY:', process.env.REACT_APP_SUPABASE_PUBLISHABLE_KEY);
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_PUBLISHABLE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
